8d55c768755bf084d76a0fb94ff41ebe768aa69f
mklaudia <39603292+mklaudia@users.noreply.github.com>
2018-06-23 11:15:21 +0200

    a button is added with hovering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { AppContainer, Title } from "./style.js";
+import { AppContainer, Title, Button } from "./style.js";
 import classNames from "classnames";
 
 import "./styles.css";
@@ -14,7 +14,7 @@ function App() {
       <h2 className={classNames("a", "b", { c: isHighlighted })}>
         Start editing to see some magic happen!
       </h2>
-      <button type="button">OK</button>
+      <Button type="button">OK</Button>
     </AppContainer>
   );
 }
--- src/style.js
+++ src/style.js
@@ -7,3 +7,10 @@ export const AppContainer = styled.div`
 export const Title = styled.h1`
   font-family: Arial, Helvetica, sans-serif;
 `;
+
+export const Button = styled.button`
+  background-color: blue;
+  &:hover {
+    background-color: #4caf50;
+  }
+`;
