f2e9439e93e69b6f34160127afd187e28974d0b0
mklaudia <39603292+mklaudia@users.noreply.github.com>
2018-06-23 11:23:00 +0200

    add pictures

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { AppContainer, Title, Button } from "./style.js";
 import classNames from "classnames";
+import styled from "styled-components";
 
 import "./styles.css";
+
+const Img = styled.div`
+  width: 100px;
+  height: 100px;
+  background-image: url(${props => props.imageSrc});
+`;
 
 function App() {
   let isHighlighted = true;
@@ -15,6 +22,7 @@ function App() {
         Start editing to see some magic happen!
       </h2>
       <Button type="button">OK</Button>
+      <Img imageSrc="https://www.w3schools.com/images/colorpicker.gif" />
     </AppContainer>
   );
 }
